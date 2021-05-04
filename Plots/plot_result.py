25d0357c86139372136d0d0b55afcf3adf5b04b4
a.gallo1412@gmail.com <Cerrelli33>
2021-05-04 16:49:59 +0200

    update plot

--- Plots/plot_result.py
+++ Plots/plot_result.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 # Access data
 directory_plot = 'C:\\Users\\agall\\OneDrive\\Desktop\\Plots\\'
 directory = 'C:\\Users\\agall\\OneDrive\\Desktop\\Results_PV2000\\'
-file = 'average_seeds.csv'
+file = 'best_seeds.csv'
 
 data = pd.read_csv(directory + file, sep=';', decimal=',')
 
@@ -43,14 +43,28 @@ def plot_bar_battery(configurations, labels, width=0.15):
 
     _, ax = plt.subplots()
 
-    ax.bar(x - width / 2, data.loc[configurations]['RL Energy consumption [kWh]'], width,
-           color='steelblue', label='RL-Consumption', ec='black')
+    # ax.bar(x - width / 2, data.loc[configurations]['RL Energy consumption [kWh]'], width,
+    #        color='darkblue', label='RL-Consumption', ec='black')
     ax.bar(x - width / 2, data.loc[configurations]['RL Energy battery [kWh]'], width,
-           color='skyblue', label='RL-Battery', ec='black')
-    ax.bar(x + width / 2, data.loc[configurations]['RBC Energy consumption [kWh]'], width,
-           color='forestgreen', label='RBC-Consumption', ec='black')
+           color='lime', label='RL-Battery', ec='black')
+    ax.bar(x - width / 2, data.loc[configurations]['RL Energy from PV [kWh]'], width,
+           bottom=data.loc[configurations]['RL Energy battery [kWh]'],
+           color='yellow', label='RL-PV', ec='black')
+    ax.bar(x - width / 2, data.loc[configurations]['RL Energy from grid [kWh]'], width,
+           bottom=data.loc[configurations]['RL Energy battery [kWh]']+data.loc[configurations]['RL Energy from PV [kWh]'],
+           color='red', label='RL-Grid', ec='black')
+
+    # ax.bar(x + width / 2, data.loc[configurations]['RBC Energy consumption [kWh]'], width,
+    #        color='cornflowerblue', label='RBC-Consumption', ec='black')
     ax.bar(x + width / 2, data.loc[configurations]['RBC Energy battery [kWh]'], width,
-           color='greenyellow', label='RBC-Battery', ec='black')
+           color='limegreen', label='RBC-Battery', ec='black')
+    ax.bar(x + width / 2, data.loc[configurations]['RBC Energy from PV [kWh]'], width,
+           bottom=data.loc[configurations]['RBC Energy battery [kWh]'],
+           color='khaki', label='RBC-PV', ec='black')
+    ax.bar(x + width / 2, data.loc[configurations]['RBC Energy from grid [kWh]'], width,
+           bottom=data.loc[configurations]['RBC Energy battery [kWh]']+data.loc[configurations]['RBC Energy from PV [kWh]'],
+           color='indianred', label='RBC-Grid', ec='black')
+
 
     # Add some text for labels, title and custom x-axis tick labels, etc.
     ax.set_xlabel('Tank volume')
@@ -62,6 +76,7 @@ def plot_bar_battery(configurations, labels, width=0.15):
     ax.set_ylim(0.0, 1500.0)
 
     plt.savefig(directory_plot + f'Energy consumption and from battery for RB and RL at {size} Wh and best seed.png')
+    plt.show()
     plt.close()
 
 
@@ -111,7 +126,7 @@ def plot_bar_tank(configurations, labels, width=0.15):
     ax.set_ylim(0.0, 1500.0)
 
     plt.savefig(directory_plot + f'Energy consumption and from battery for RB and RL at {size} m^3 and best seed.png')
-
+    plt.show()
     plt.close()
 ########################################################################################################################
 
@@ -139,3 +154,4 @@ plot_bar_tank(configuration, labels_energy)
 # Tank 6, Configuration
 configuration = [6, 7, 8]
 plot_bar_tank(configuration, labels_energy)
+
